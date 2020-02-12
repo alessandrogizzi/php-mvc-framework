81c9fd9193e8b0b58f9011acd221889803168fb6
carlopisacane <doncarlopisacane@gmail.com>
2020-02-12 16:54:50 +0100

    the database class

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,12 @@
 <?php
+// DB Params
+define('DB_HOST', 'mysql:3306');
+define('DB_USER', 'root');
+define('DB_PASS', 'tiger');
+define('DB_NAME', 'tmvc');
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
 define('URLROOT', 'http://localhost:60/app1/');
 //Site Name
-define('SITENAME', 'phpMVC' );
+define('SITENAME', 'phpMVC');
