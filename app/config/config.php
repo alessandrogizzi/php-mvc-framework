ec2861fe2790ff397087500656107567af83beae
carlopisacane <doncarlopisacane@gmail.com>
2020-02-12 17:03:31 +0100

    Clean Up

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 // DB Params
 define('DB_HOST', 'mysql:3306');
-define('DB_USER', 'root');
-define('DB_PASS', 'tiger');
-define('DB_NAME', 'tmvc');
+define('DB_USER', '_YOUR_USER_');
+define('DB_PASS', '_YOUR_PASS_');
+define('DB_NAME', '_YOUR_DBNAME_');
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost:60/app1/');
+define('URLROOT', '_YOUR_URL_');
 //Site Name
-define('SITENAME', 'phpMVC');
+define('SITENAME', '_YOUR_SITENAME_');
