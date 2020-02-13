92eae1b6d301295d252cb02033193f66e642ca04
carlopisacane <doncarlopisacane@gmail.com>
2020-02-13 18:35:52 +0100

    User session data & logout

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -18,7 +18,9 @@ function flash($name = '', $message = '', $class = 'alert alert-success')
 
    $_SESSION[$name]            = $message;
    $_SESSION[$name . '_class'] = $class;
+
   } elseif (empty($message) && !empty($_SESSION[$name])) {
+
    $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
    echo '<div class="' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
    unset($_SESSION[$name]);
