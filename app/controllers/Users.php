308c374aa90bc55f4c9ec8c6f3e655177717d373
carlopisacane <doncarlopisacane@gmail.com>
2020-02-13 20:58:21 +0100

    posts access control

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -168,7 +168,7 @@ class Users extends Controller
   $_SESSION['user_email'] = $user->email;
   $_SESSION['user_name']  = $user->name;
 
-  redirect('pages/index');
+  redirect('posts');
  }
 
  public function logout()
@@ -180,12 +180,4 @@ class Users extends Controller
   redirect('users/login');
  }
 
- public function isLoggedIn()
- {
-  if (isset($_SESSION['user_id'])) {
-   return true;
-  } else {
-   return false;
-  }
- }
 }
